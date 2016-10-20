65a27be361b5e795fa5f03c702714306b45da23e
jai1 <jai.asher88@gmail.com>
2016-10-20 22:52:46 +0100

    Added the base skeleton, module declaration and the routing config

---
+++ public/assignment/app.js
@@ -0,0 +1,6 @@
+/* Declaration for the name of the module and the it's dependencies - which in this case is ngRoute (for routing) */
+
+(function () {
+    angular
+        .module("WebAppMaker", ['ngRoute']);
+})();
