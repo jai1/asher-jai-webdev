ed8e5c707b2a568bd9b306865a55fc5450942691
jai1 <jai.asher88@gmail.com>
2016-11-28 18:41:07 -0500

    Completed homepage Homepage

--- public/homepage/mail/contact_me.php
+++ public/homepage/mail/contact_me.php
@@ -16,11 +16,17 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'jai_asher123@yahoo.co.in'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$mail=mail($to, "Subject: $email_subject",$message );
+if($mail){
+    echo "Thank you for using our mail form";
+    return true;
+}else{
+    echo "Mail sending failed.";
+    return false;
+}
 ?>
